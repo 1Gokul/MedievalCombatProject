13dcee0def86413474d30256792314c51a77119a
Gokul Viswanath <viswanath1gokul@gmail.com>
2020-04-05 23:25:12 +0530

    FloorSwitch added
    Uses a Triggerbox to make a door move up with an animation

---
+++ TestProject/Source/TestProject/FloorSwitch.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloorSwitch.generated.h"
+
+UCLASS()
+class TESTPROJECT_API AFloorSwitch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloorSwitch();
+
+	/** Overlap volume for functionality to be triggered*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = FloorSwitch)
+		class UBoxComponent* TriggerBox;
+
+	/** Switch for character to step on*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = FloorSwitch)
+		class UStaticMeshComponent* FloorSwitch;
+
+	/** Door that moves when the switch is stepped on*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = FloorSwitch)
+		class UStaticMeshComponent* Door;
+
+	/** Initial Location of Door*/
+	UPROPERTY(BlueprintReadWrite, Category = FloorSwitch)
+		FVector InitialDoorLocation;
+
+	/** Initial Location of Floor Switch*/
+	UPROPERTY(BlueprintReadWrite, Category = FloorSwitch)
+		FVector InitialSwitchLocation;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = FloorSwitch)
+		void RaiseDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = FloorSwitch)
+		void LowerDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = FloorSwitch)
+		void RaiseFloorSwitch();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = FloorSwitch)
+		void LowerFloorSwitch();
+
+	UFUNCTION(BlueprintCallable, Category = FloorSwitch)
+		void UpdateDoorLocation(float Z);
+
+	UFUNCTION(BlueprintCallable, Category = FloorSwitch)
+		void UpdateFloorSwitchLocation(float Z);
+
+};
---
+++ TestProject/Source/TestProject/MainAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTPROJECT_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+		void UpdateAnimationProperties();
+	
+	virtual void NativeInitializeAnimation() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+		float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+		bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement)
+		class APawn* Pawn;
+	
+};
